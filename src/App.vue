a4f99cb9f1d7c3041bb9858b247748059c0572ec
joannecya <e0421141@u.nus.edu>
2022-04-04 15:53:36 +0800

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,23 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <RandomNumber msg="Welcome to Unit Testing" />
 </template>
-
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
-export default {
+  import RandomNumber from "./components/RandomNumber.vue";
+  export default {
   name: "App",
   components: {
-    HelloWorld,
+  RandomNumber,
   },
-};
+  };
 </script>
 
 <style>
-#app {
+  #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
-}
+  }
 </style>
--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -1,12 +1,30 @@
 import { shallowMount } from "@vue/test-utils";
-import HelloWorld from "@/components/HelloWorld.vue";
+import RandomNumber from "@/components/RandomNumber.vue";
 
-describe("HelloWorld.vue", () => {
-  it("renders props.msg when passed", () => {
-    const msg = "new message";
-    const wrapper = shallowMount(HelloWorld, {
-      props: { msg },
-    });
-    expect(wrapper.text()).toMatch(msg);
+describe("RandomNumber.vue", () => {
+  it("BY default the random number should be 0", () => {
+    /* const msg = "new message"; */
+    const wrapper = shallowMount(RandomNumber);
+    expect(wrapper.html()).toContain('0');
   });
 });
+
+test('if BUTTON CLICKED the random number should be 1-10', async () => {
+  const wrapper = shallowMount(RandomNumber)
+  await wrapper.find('button').trigger('click')
+  const random_number = parseInt(wrapper.find('h2').text())
+  expect(random_number).toBeGreaterThanOrEqual(1)
+  expect(random_number).toBeLessThanOrEqual(10)
+})
+
+test('if BUTTON CLICKED the random number should be 50-100', async () => {
+  const wrapper = shallowMount(RandomNumber,{
+      props:{
+          min:50, max:100
+      }
+  })
+  await wrapper.find('button').trigger('click')
+  const random_number = parseInt(wrapper.find('h2').text())
+  expect(random_number).toBeGreaterThanOrEqual(50)
+  expect(random_number).toBeLessThanOrEqual(100)
+})
